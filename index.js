2434633bdefa3fd948b3938cc27e2980587b99ac
Bruno Soaress 🍔 <bruno@stoodi.com.br>
2018-06-15 11:24:35 -0300

    Change identation to 4 spaces.

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 'use strict';
 
 let express = require('express'),
-  bodyParser = require('body-parser'),
-  request = require('request'),
-  app = express(),
-  config = require('config'),
-  images = require('./pics.js');
+    bodyParser = require('body-parser'),
+    request = require('request'),
+    app = express(),
+    config = require('config'),
+    images = require('./pics.js');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -16,194 +16,195 @@ app.get('/', (req, res) => res.send('Hello World!'));
 
 // Adds support for GET requests to our webhook
 app.get('/webhook', (req, res) => {
-  // Your verify token. Should be a random string.
-  let VERIFY_TOKEN = 'bruno@2018';
+    // Your verify token. Should be a random string.
+    let VERIFY_TOKEN = 'bruno@2018';
 
-  // Parse the query params
-  let mode = req.query['hub.mode'];
-  let token = req.query['hub.verify_token'];
-  let challenge = req.query['hub.challenge'];
+    // Parse the query params
+    let mode = req.query['hub.mode'];
+    let token = req.query['hub.verify_token'];
+    let challenge = req.query['hub.challenge'];
 
-  // Checks if a token and mode is in the query string of the request
-  if (mode && token) {
-    // Checks the mode and token sent is correct
-    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-      // Responds with the challenge token from the request
-      console.log('WEBHOOK_VERIFIED');
-      res.status(200).send(challenge);
-    } else {
-      // Responds with '403 Forbidden' if verify tokens do not match
-      res.sendStatus(403);
+    // Checks if a token and mode is in the query string of the request
+    if (mode && token) {
+        // Checks the mode and token sent is correct
+        if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+            // Responds with the challenge token from the request
+            console.log('WEBHOOK_VERIFIED');
+            res.status(200).send(challenge);
+        } else {
+            // Responds with '403 Forbidden' if verify tokens do not match
+            res.sendStatus(403);
+        }
     }
-  }
 });
 
 app.post('/webhook', (req, res) => {
-  let body = req.body;
-  if (body.object === 'page') {
-    // Iterates over each entry - there may be multiple if batched
-    body.entry.forEach(function(entry) {
-      // Gets the message. entry.messaging is an array, but
-      // will only ever contain one message, so we get index 0
-      let webhook_event = entry.messaging[0];
-      // console.log(webhook_event);
+    let body = req.body;
+    if (body.object === 'page') {
+        // Iterates over each entry - there may be multiple if batched
+        body.entry.forEach(function(entry) {
+            // Gets the message. entry.messaging is an array, but
+            // will only ever contain one message, so we get index 0
+            let webhook_event = entry.messaging[0];
+            // console.log(webhook_event);
 
-      // Get the sender PSID
-      let sender_psid = webhook_event.sender.id;
-      // console.log('Sender PSID: ' + sender_psid);
+            // Get the sender PSID
+            let sender_psid = webhook_event.sender.id;
+            // console.log('Sender PSID: ' + sender_psid);
 
-      // Check if the event is a message or postback and
-      // pass the event to the appropriate handler function
-      if (webhook_event.message) {
-        handleMessage(sender_psid, webhook_event.message);
-        console.log('SEND MESSAGE');
-        // console.log(webhook_event.message);
-      } else if (webhook_event.postback) {
-        console.log('POSTBACK MESSAGE');
-        handlePostback(sender_psid, webhook_event.postback);
-        // console.log(webhook_event.postback);
-      }
-    });
+            // Check if the event is a message or postback and
+            // pass the event to the appropriate handler function
+            if (webhook_event.message) {
+                handleMessage(sender_psid, webhook_event.message);
+                console.log('SEND MESSAGE');
+                // console.log(webhook_event.message);
+            } else if (webhook_event.postback) {
+                console.log('POSTBACK MESSAGE');
+                handlePostback(sender_psid, webhook_event.postback);
+                // console.log(webhook_event.postback);
+            }
+        });
 
-    // Returns a '200 OK' response to all requests
-    res.status(200).send('EVENT_RECEIVED');
-  } else {
-    // Returns a '404 Not Found' if event is not from a page subscription
-    res.sendStatus(404);
-  }
+        // Returns a '200 OK' response to all requests
+        res.status(200).send('EVENT_RECEIVED');
+    } else {
+        // Returns a '404 Not Found' if event is not from a page subscription
+        res.sendStatus(404);
+    }
 });
 
 // Handles messages events
 const handleMessage = (sender_psid, received_message) => {
-  let response;
+    let response;
 
-  // Check if the message contains text
-  if (received_message.text) {
-    // Create the payload for a basic text message
-    response = askTemplate('Are you a Cat or Dog Person?');
-  }
+    // Check if the message contains text
+    if (received_message.text) {
+        // Create the payload for a basic text message
+        response = askTemplate('Are you a Cat or Dog Person?');
+    }
 
-  // Sends the response message
-  callSendAPI(sender_psid, response);
+    // Sends the response message
+    callSendAPI(sender_psid, response);
 };
 
 const handlePostback = (sender_psid, received_postback) => {
-  let response;
+    let response;
 
-  // Get the payload for the postback
-  let payload = received_postback.payload;
+    // Get the payload for the postback
+    let payload = received_postback.payload;
 
-  // Set the response based on the postback payload
-  if (payload === 'BURGERS_PICS') {
-    response = imageTemplate('burgers', sender_psid);
-    callSendAPI(sender_psid, response, function() {
-      callSendAPI(sender_psid, askTemplate('Show me more'));
-    });
-  } else if (payload === 'DOG_PICS') {
-    response = imageTemplate('dogs', sender_psid);
-    callSendAPI(sender_psid, response, function() {
-      callSendAPI(sender_psid, askTemplate('Show me more'));
-    });
-  } else if (payload === 'GET_STARTED') {
-    response = askTemplate('Are you a Cat or Dog Person?');
-    callSendAPI(sender_psid, response);
-  }
-  // Send the message to acknowledge the postback
+    // Set the response based on the postback payload
+    if (payload === 'BURGERS_PICS') {
+        response = imageTemplate('burgers', sender_psid);
+        callSendAPI(sender_psid, response, function() {
+            callSendAPI(sender_psid, askTemplate('Show me more'));
+        });
+    } else if (payload === 'DOG_PICS') {
+        response = imageTemplate('dogs', sender_psid);
+        callSendAPI(sender_psid, response, function() {
+            callSendAPI(sender_psid, askTemplate('Show me more'));
+        });
+    } else if (payload === 'GET_STARTED') {
+        response = askTemplate('Are you a Cat or Dog Person?');
+        callSendAPI(sender_psid, response);
+    }
+    // Send the message to acknowledge the postback
 };
 
 // Sends response messages via the Send API
 const callSendAPI = (sender_psid, response, cb = null) => {
-  // Construct the message body
-  let request_body = {
-    recipient: {
-      id: sender_psid
-    },
-    message: response
-  };
+    // Construct the message body
+    let request_body = {
+        recipient: {
+            id: sender_psid
+        },
+        message: response
+    };
 
-  // Send the HTTP request to the Messenger Platform
-  request(
-    {
-      uri: 'https://graph.facebook.com/v2.6/me/messages',
-      qs: { access_token: config.get('facebook.page.access_token') },
-      method: 'POST',
-      json: request_body
-    },
-    (err, res, body) => {
-      if (!err) {
-        if (cb) {
-          cb();
+    // Send the HTTP request to the Messenger Platform
+    request(
+        {
+            uri: 'https://graph.facebook.com/v2.6/me/messages',
+            qs: { access_token: config.get('facebook.page.access_token') },
+            method: 'POST',
+            json: request_body
+        },
+        (err, res, body) => {
+            if (!err) {
+                if (cb) {
+                    cb();
+                }
+            } else {
+                console.error('Unable to send message:' + err);
+            }
         }
-      } else {
-        console.error('Unable to send message:' + err);
-      }
-    }
-  );
+    );
 };
 
 const imageTemplate = (type, sender_id) => {
-  return {
-    attachment: {
-      type: 'image',
-      payload: {
-        url: getImage(type, sender_id),
-        is_reusable: true
-      }
-    }
-  };
+    return {
+        attachment: {
+            type: 'image',
+            payload: {
+                url: getImage(type, sender_id),
+                is_reusable: true
+            }
+        }
+    };
 };
 
 const getImage = (type, sender_id) => {
-  // create user if doesn't exist
-  if (users[sender_id] === undefined) {
-    users = Object.assign(
-      {
-        [sender_id]: {
-          burgers_count: 0,
-          dogs_count: 0
-        }
-      },
-      users
-    );
-  }
+    // create user if doesn't exist
+    if (users[sender_id] === undefined) {
+        users = Object.assign(
+            {
+                [sender_id]: {
+                    burgers_count: 0,
+                    dogs_count: 0
+                }
+            },
+            users
+        );
+    }
 
-  let count = images[type].length, // total available images by type
-    user = users[sender_id], // // user requesting image
-    user_type_count = user[type + '_count'];
+    let count = images[type].length, // total available images by type
+        user = users[sender_id], // // user requesting image
+        user_type_count = user[type + '_count'];
 
-  // update user before returning image
-  let updated_user = {
-    [sender_id]: Object.assign(user, {
-      [type + '_count']: count === user_type_count + 1 ? 0 : user_type_count + 1
-    })
-  };
-  // update users
-  users = Object.assign(users, updated_user);
+    // update user before returning image
+    let updated_user = {
+        [sender_id]: Object.assign(user, {
+            [type + '_count']:
+                count === user_type_count + 1 ? 0 : user_type_count + 1
+        })
+    };
+    // update users
+    users = Object.assign(users, updated_user);
 
-  // console.log(users);
-  return images[type][user_type_count];
+    // console.log(users);
+    return images[type][user_type_count];
 };
 
 const askTemplate = text => {
-  return {
-    attachment: {
-      type: 'template',
-      payload: {
-        template_type: 'button',
-        text: text,
-        buttons: [
-          {
-            type: 'postback',
-            title: 'Burgers',
-            payload: 'BURGERS_PICS'
-          },
-          {
-            type: 'postback',
-            title: 'Dogs',
-            payload: 'DOG_PICS'
-          }
-        ]
-      }
-    }
-  };
+    return {
+        attachment: {
+            type: 'template',
+            payload: {
+                template_type: 'button',
+                text: text,
+                buttons: [
+                    {
+                        type: 'postback',
+                        title: 'Burgers',
+                        payload: 'BURGERS_PICS'
+                    },
+                    {
+                        type: 'postback',
+                        title: 'Dogs',
+                        payload: 'DOG_PICS'
+                    }
+                ]
+            }
+        }
+    };
 };
--- pics.js
+++ pics.js
@@ -1,24 +1,24 @@
 module.exports = {
-  burgers: [
-    'https://foodal.com/wp-content/uploads/2015/07/5-Tasty-Ideas-to-Liven-Up-Your-Burgers.jpg',
-    'https://foodal.com/wp-content/uploads/2015/07/5-Great-Hamburger-Ideas.jpg',
-    'https://foodal.com/wp-content/uploads/2015/07/Hamburger-Tips-and-Variations.jpg',
-    'https://foodal.com/wp-content/uploads/2015/07/Oozy-Bluesy-Stilton-and-Sirloin-Burger-Recipe.jpg',
-    'https://foodal.com/wp-content/uploads/2015/07/Recipe-for-Oozy-Bluesy-Stilton-and-Sirloin-Burgers.jpg',
-    'https://foodal.com/wp-content/uploads/2015/07/Homemade-Turkey-Burger-Recipe.jpg'
-  ],
+    burgers: [
+        'https://foodal.com/wp-content/uploads/2015/07/5-Tasty-Ideas-to-Liven-Up-Your-Burgers.jpg',
+        'https://foodal.com/wp-content/uploads/2015/07/5-Great-Hamburger-Ideas.jpg',
+        'https://foodal.com/wp-content/uploads/2015/07/Hamburger-Tips-and-Variations.jpg',
+        'https://foodal.com/wp-content/uploads/2015/07/Oozy-Bluesy-Stilton-and-Sirloin-Burger-Recipe.jpg',
+        'https://foodal.com/wp-content/uploads/2015/07/Recipe-for-Oozy-Bluesy-Stilton-and-Sirloin-Burgers.jpg',
+        'https://foodal.com/wp-content/uploads/2015/07/Homemade-Turkey-Burger-Recipe.jpg'
+    ],
 
-  dogs: [
-    'https://i.redd.it/6tjihi2qe7111.jpg',
-    'https://i.imgur.com/etRCs56.jpg',
-    'https://i.redd.it/nibw50f8y4111.jpg',
-    'https://i.redd.it/izcvnvj1o7111.jpg',
-    'https://i.redd.it/eqs1g9dldz011.jpg',
-    'https://i.redd.it/civ9dnu9u1111.jpg',
-    'https://i.redd.it/kk03qwclkp011.jpg',
-    'https://i.redd.it/2694pupjne011.jpg',
-    'https://i.redd.it/qk49ls5y6oy01.jpg',
-    'https://i.imgur.com/oM3mKgB.jpg',
-    'https://i.redd.it/8kx2riaulux01.jpg'
-  ]
+    dogs: [
+        'https://i.redd.it/6tjihi2qe7111.jpg',
+        'https://i.imgur.com/etRCs56.jpg',
+        'https://i.redd.it/nibw50f8y4111.jpg',
+        'https://i.redd.it/izcvnvj1o7111.jpg',
+        'https://i.redd.it/eqs1g9dldz011.jpg',
+        'https://i.redd.it/civ9dnu9u1111.jpg',
+        'https://i.redd.it/kk03qwclkp011.jpg',
+        'https://i.redd.it/2694pupjne011.jpg',
+        'https://i.redd.it/qk49ls5y6oy01.jpg',
+        'https://i.imgur.com/oM3mKgB.jpg',
+        'https://i.redd.it/8kx2riaulux01.jpg'
+    ]
 };
